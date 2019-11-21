e77f1f0b47503bee7d6922d9c0a792554cac0d91
eriqueg <guizanoni1234@gmail.com>
2019-11-21 01:10:50 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/unisep/spotifree/SpotifreeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unisep.spotifree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/unisep/spotifree/SpotifreeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unisep.spotifree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
