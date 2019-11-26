619b4ed75c2cda03088dff116f541a4954f0fa1f
eriqueg <guizanoni1234@gmail.com>
2019-11-26 01:27:53 -0300

    Criando User

--- src/main/java/br/edu/unisep/spotifree/model/entity/user.java
+++ src/main/java/br/edu/unisep/spotifree/model/entity/user.java
@@ -1,4 +1,36 @@
 package br.edu.unisep.spotifree.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "user")
+@Builder
 public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+//    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+//    private List<User>
+
 }
