c9178818fddea8a242980d8786dd5ad715166eb1
eriqueg <guizanoni1234@gmail.com>
2019-11-28 01:35:15 -0300

    Making JwtEntryPoint

--- src/main/java/br/edu/unisep/spotifree/security/JwtEntryPoint.java
+++ src/main/java/br/edu/unisep/spotifree/security/JwtEntryPoint.java
@@ -2,12 +2,14 @@ package br.edu.unisep.spotifree.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
